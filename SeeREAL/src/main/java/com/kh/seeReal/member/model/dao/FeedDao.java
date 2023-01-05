74c21f33f2df9680c02ae0a93892f5899b0a23b5
YEJINK072 <121650317+YEJINK072@users.noreply.github.com>
2023-01-05 10:29:38 +0900

    Feed Controller,Dao,Service 만듬 & annotation 연결 -예진-

---
+++ SeeREAL/src/main/java/com/kh/seeReal/member/model/dao/FeedDao.java
@@ -0,0 +1,8 @@
+package com.kh.seeReal.member.model.dao;
+
+import org.springframework.stereotype.Repository;
+
+@Repository
+public class FeedDao {
+
+}
---
+++ SeeREAL/src/main/java/com/kh/seeReal/member/model/service/FeedServiceImp.java
@@ -0,0 +1,8 @@
+package com.kh.seeReal.member.model.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class FeedServiceImp implements FeedService{
+
+}
