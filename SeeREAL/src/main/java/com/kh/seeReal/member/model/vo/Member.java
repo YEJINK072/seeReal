2d46ede23c1e3b3c7ffef5969a52e2ee7924917f
ykl09180918 <121650525+ykl09180918@users.noreply.github.com>
2023-01-05 09:21:58 +0900

    member필드변경 - 유경
    아이디 필드 삭제
    닉네임 필드 생성

--- SeeREAL/src/main/java/com/kh/seeReal/member/model/vo/Member.java
+++ SeeREAL/src/main/java/com/kh/seeReal/member/model/vo/Member.java
@@ -8,10 +8,10 @@ import lombok.Data;
 public class Member {
 
 	private int memberNo; 		//MEMBER_NO
-	private String memberId;	//MEMBER_ID
+	private String memberEmail;	// 이메일을 아이디로 사용
+	private String memberNickname;	// 닉네임
 	private String memberPwd;	//MEMBER_PWD
 	private String memberPhone;	//MEMBER_PHONE
-	private String memberEmail;	//MEMBER_EMAIL
 	private String memberPhoto;	//MEMBER_PHOTO
 	private Date memberCreateDate;	//MEMBER_CREATEDATE
 	private Date memberModifyDate;	//MEMBER_MODIFYDATE
